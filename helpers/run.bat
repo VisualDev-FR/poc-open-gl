b3e3bcbcd0789b2d6c6b81ef3acf936ac54d3499
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-11-30 18:29:36 +0100

    setup modular app name

--- helpers/run.bat
+++ helpers/run.bat
@@ -1,9 +1,11 @@
 @echo off
 
+SET APP_NAME=program
+
 cd .\build
 
-cmake.exe -S .. -B . -G "MinGW Makefiles"
+cmake.exe -S .. -B . -G "MinGW Makefiles" -DAPP_NAME=%APP_NAME%
 
 mingw32-make.exe
 
-.\MonProjet.exe
+.\program.exe
